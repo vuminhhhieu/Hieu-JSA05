484917763a02ab07c5d213a8cd48dcd085518b38
vuminhhieu <vmhieu8.1@gmail.com>
2023-08-13 12:04:02 +0700

    buổi 2

---
+++ javascript.js
@@ -0,0 +1,34 @@
+// let x = 0;
+// while (x<10) {
+//     console.log(x += 1);
+// }
+
+// do {alert(x += 1);} while(x<10);
+
+// for (var i = 0; i < 10; i++) {alert(i)}
+
+let myNumber = [0 ,1 ,2];
+let myString = ["Hiếu", "Ngọc", "Tài"];
+
+// console.log(myString[2]);
+
+// console.log(myString);
+// console.log(myNumber);
+
+// for (var i = 0; i < 3; i++) {
+//     alert(i+":"+myString[i]);
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     console.log(i+":"+myString[i]);
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(i + ":" + myString[1]);
+    i++;
+}
+while (i < 3);
+
