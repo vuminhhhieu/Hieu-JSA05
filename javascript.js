aaf61e5f58524c135667b28cb1014af5879943d6
vuminhhieu <vmhieu8.1@gmail.com>
2023-08-20 11:27:05 +0700

    buổi 2

--- javascript.js
+++ javascript.js
@@ -7,8 +7,8 @@
 
 // for (var i = 0; i < 10; i++) {alert(i)}
 
-let myNumber = [0 ,1 ,2];
-let myString = ["Hiếu", "Ngọc", "Tài"];
+// let myNumber = [0 ,1 ,2];
+// let myString = ["Hiếu", "Ngọc", "Tài"];
 
 // console.log(myString[2]);
 
@@ -25,10 +25,89 @@ let myString = ["Hiếu", "Ngọc", "Tài"];
 //     i++;
 // }
 
-let i = 0;
-do {
-    console.log(i + ":" + myString[1]);
-    i++;
+// let i = 0;
+// do {
+//     console.log(i + ":" + myString[1]);
+//     i++;
+// }
+// while (i < 3);
+
+let myArray = [4, 5, 7, 8, 9, 1,]
+console.log(myArray)
+
+// for( let i = 0; i < myArray.length; i++ ) {
+//     console.log("Giá trị của phân tử thứ", i, " là: ", myArray[i] )
+// }
+
+// myArray[2] = 6;
+// console.log(myArray)
+
+// myArray.splice(2, 1, 7, 8);
+// console.log(myArray)
+
+// console.log(myArray.length);
+
+// myArray.push(7)
+// console.log(myArray)
+
+// myArray.pop()
+// console.log(myArray)
+
+// myArray.unshift(9)
+// console.log(myArray)
+
+// myArray.shift()
+// console.log(myArray)
+
+let dog = {
+    name: 'Alaska',
+    age: 4
 }
-while (i < 3);
+
+// dog.name = 'Chihuahua'
+// console.log(dog);
+
+// dog.action = 'Walk'
+// console.log(dog)
+
+// delete ( dog.action )
+// console.log(dog)
+
+// let cat = new Object({
+//     name: 'Tom',
+//     age: 3,
+//     enemy: {
+//         name: 'Alaska',
+//         age:5
+//     }
+
+// })
+// console.log(cat);
+
+// let dog1 = ["Alaska", 4]
+// let dog2 = ["Corgi", 5]
+
+// let dogs = [dog1, dog2]
+
+// console.log(dogs)
+
+// console.log( dogs[0][1] )
+
+let gioTraiCay = [
+    ['xoai', 2],
+    ['dua hau', 3],
+]
+
+for (let i = 0; i<gioTraiCay.length; i++) {
+    console.log("Gio trai cay: " + gioTraiCay[i][0] + gioTraiCay[i][1])
+}
+
+
+
+
+
+
+
+
+
 
