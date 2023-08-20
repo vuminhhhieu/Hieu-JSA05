49af17f741f636ba48ab703f951b5f82adc30446
vuminhhieu <vmhieu8.1@gmail.com>
2023-08-20 12:05:44 +0700

    buổi 3

---
+++ buổi 3/javascript.js
@@ -0,0 +1,17 @@
+let arr = ["Toán", "Ngữ Văn", "Tiếng Anh"]
+
+// for (let i in arr){
+//     console.log(arr[i])
+// }
+
+for (let value of arr)
+console.log(value)
+
+// let obj = {
+//     'name': "Nguyễn Văn Đẩu",
+//     'sex':"Không xác định",
+//     'crime': "Vi phạm hành chính"
+// }
+
+// for (let i in obj)
+// console.log(obj[i])
